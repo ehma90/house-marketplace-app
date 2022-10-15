a273538237d28c98d85575a2f499f75ec6adc2ee
Emmanuel Essien <ehmaessien@gmail.com>
2022-10-15 22:43:47 +0100

    Create pages folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Profile from './pages/Profile'
+import Offer from './pages/Offer'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+import Explore from './pages/Explore'
 
 function App() {
   return (
-    <div className="App">
-      <h1>My App</h1>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Explore/>}/>
+        <Route path='/offer' element={<Offer/>}/>
+        <Route path='/profile' element={<SignIn/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/forgot-password' element={<ForgotPassword/>}/>
+      </Routes>
+    </Router>
+    {/* Navbar */}
+    </>
   );
 }
 
